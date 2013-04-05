ad9f2d6a39905be81732b0ef993a752afc440fb3
Maximiliano Báez González <mxbg.py@gmail.com>
2013-04-05 18:16:23 -0300

    Se añade el  archivo de configuraciones

---
+++ src/config.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#~ Configuraciones para la conexión con la base de datos
+DB = {
+    "dbname" : "dengue",
+    "host": "localhost",
+    "port": "5432",
+    "user": "postgres",
+    "password": "postgres"
+}
+
+TMP_HOME = "./data/"
+RASTER = {
+    "schema": "public",
+    "table" : "tabla",
+    "srid" : "900913",
+    "pk" : "id"
+}
+#~ Configuraciones para la conexión con el geoserver.
+GEOSERVER = {
+    "host":"localhost",
+    "port":"8080",
+    "user": "admin",
+    "password": "geoserver"
+}

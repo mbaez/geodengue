5bb610ffb1d991d2d693eee71faca1419ba958b6
rbanuelos <robertobanuelos@gmail.com>
2013-07-21 11:18:40 -0400

    Se agregaro un nuevo directorio para la DB. Se agregaron scripts sql para la creacion y modificacion de la DB

---
+++ db/alter-puntos-de-control.sql
@@ -0,0 +1,15 @@
+﻿-- Table: puntos_control
+
+-- Script para agregar las columnas 
+-- de fecha de recoleccion y instalacion
+-- a la entidad punto de control
+
+ALTER TABLE 
+	puntos_control
+ADD COLUMN 
+	fecha_recoleccion date;
+  
+ALTER TABLE 
+	puntos_control 
+ADD COLUMN 
+	fecha_instalacion date; 

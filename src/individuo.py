1fe3b986a91ad022760d10801eeb1a502a8103e8
Maximiliano Báez González <mxbg.py@gmail.com>
2013-08-27 01:22:19 -0400

    Se implementa el proceso de desarrollo para las pupas teniendo en cuenta las variables climatologicas

--- src/individuo.py
+++ src/individuo.py
@@ -9,7 +9,6 @@ Larva, Pupa, Adulto) para, finalmente, representar a un individuo.
 """
 #Se impotan los modulos.
 from models import *
-
 from random import randint
 """
 Enum que representa los estados por lo cuales atravieza el individuo.
@@ -189,11 +188,19 @@ class Huevo(AeAegypti) :
         self._edad +=1
         return self;
 
-
 class Larva(AeAegypti) :
     """
     Esta clase contiene la representación del AeAegypti en su etapa de
     larva.
+
+    Los porcentajes (y rango) del tiempo total de desarrollo, en todo el
+    rango de temperaturas, que pasó en cada etapa inmadura son :
+    1er estadio : 19% (13,9 a 23,1)
+    2do estadio : 14% (11,3 a 21,3)
+    3er estadio : 17% (13,0 a 27,0)
+    4to estadio : 27% (24,2 a 28,5)
+    Fuente : Temperature-Dependent Development and Survival Rates of
+    Culex quinquefasciatus tus and Aedes aegypti (Diptera: Culicidae
     """
     def __init__(self, previous_sexo=None) :
         """
@@ -320,7 +327,18 @@ class Larva(AeAegypti) :
 class Pupa(AeAegypti) :
     """
     Esta clase contiene la representación del AeAegypti en su etapa de
-    pupa.
+    pupa. La etapa de pupa es una etapa de no alimentación, se mueven,
+    respondiendo a cambios en la luz y al movimiento. Durante esta etapa,
+    el mosquito se transforma en adulto. En mosquitos Culex esto toma 2
+    días en el verano. Las pupas de mosquito viven en el agua de 1 a 4
+    días, según la especie y la temperatura.
+
+    La metamorfosis del mosquito en adulto se completa dentro de la cavidad
+    de la pupa. Cuando el desarrollo está terminado, la piel de la pupa se
+    rompe y el mosquito adulto emerge
+
+    @see http://medicina.tij.uabc.mx/von/ciclo.html
+    @see http://www.todosobremosquitos.com.ar/index.php?id=48&titulo=pupas-de-mosquitos
     """
     def __init__(self, previous_sexo) :
         """
@@ -332,32 +350,107 @@ class Pupa(AeAegypti) :
 
     def esta_muerto (self):
         """
-        La supervivencia de los mosquitos depende de la capacidad para alimentarse,
-        reproducirse, protegerse y dispersarse.
-            pupa    1 a 4 dias
+        El estadio de pupa dura aproximadamente dos o tres días, emergiendo
+        alrededor del 88% de los adultos en cuestión de 48 horas (Méndez et al., 1996)
+        @see Dengeue en Mexico
+
         """
         return (self.espectativa_vida <= 0 or self.edad > 4*24 )
 
     def desarrollar(self, hora) :
         """
-        Este método se encarga de desarrollar el individuo que se encuentra
-        en el estado de pupa.
+        El estadio de pupa dura aproximadamente dos o tres días, emergiendo
+        alrededor del 88% de los adultos en cuestión de 48 horas (Méndez et al., 1996)
 
-        El estado de pupa demora de 2 a 3 días.
+        Entre los 27-32oC la pupa que da lugar al Adulto macho emerge en
+        1,9 días y la pupa que originará una hembra muda en 2,5 días.
+
+        La duración promedio de la pupa, expresada como porcentaje del
+        tiempo ocupado por la larva hasta que llega a adulto es de 20,6
+        (Baz-Zeed, 1958)
+
+        [26,83 (10-47)] * 0.206 días a 20oC
+        [17,59 (9-29)]  * 0.206 días a 25oC
+        [9,75 (5-16) ] * 0.206  días a 30oC
+
+        @see Dengeue en Mexico
+        @see El Aedes aegypti y la transmision del dengue
+        @see The effect of temperature on the growth rate and survival
+             of the immature stages of Aedes aegypti
         """
         #~ se verifica si el individuo puede realizar un cambio de estado
-        if self.edad < 3*24 :
-            estado = randint(2, 4) * 24
-            if estado <= self.edad :
-                print str(self)
-                return Adulto(self.sexo)
+        if self.madurez >= 100 :
+            print str(self)
+            return Adulto(self.sexo)
+
+        #~  se inicializa la varible
+        delta_madurez = 0.0
+        if hora.temperatura >= 27  and hora.temperatura <= 32:
+            """
+            Entre los 27-32oC la pupa se transforma a un adulto, escenario
+            ideal :
+
+            Macho 1.9 días : delta_madurez = 100 /(1.9 * 24)
+            Hembra 2.5 días: delta_madurez = 100 /(2.5 * 24)
+
+            TODO : ¿Que pasa cuando la temperatura supera los 32 C ?
+            """
+            #~ Si es hembra el proceso de madurez tarda más
+            if self.sexo == Sexo.HEMBRA :
+                delta_madurez = 100/(2.5 * 24.0)
+            else :
+                delta_madurez = 100/(1.9 * 24)
+
+        elif hora.temperatura > 20  and hora.temperatura <= 25:
+            """
+            [17,59 (9-29)] * 0.206 días a 25oC
+            Para determinar  la cantidad de días que toma a un macho y
+            una hembra se partio del punto que para el desarrollo, en
+            condiciones optimas se dearrolla un
+            Macho 1.9 días
+            Hembra 2.5 días
+
+            Suponiendo que el estado por defecto es la hembra se calcula
+            la relación:
+
+            Macho = 2.5 / 1.9 Hembras = 1,315789474 Hembras
+
+            """
+            delta_madurez = 100.0 / (randint(9, 29) * 0.206 * 24.0)
+            #~  si es macho madura más rápido
+            if sexo.MACHO == self.sexo :
+                delta_madurez =  delta_madurez / 1.315789474
+
+        elif hora.temperatura <= 20:
+            """
+            [26,83 (10-47)] * 0.206 días a 20oC
+
+            Para determinar  la cantidad de días que toma a un macho y
+            una hembra se partio del punto que para el desarrollo, en
+            condiciones optimas se dearrolla un
+            Macho 1.9 días
+            Hembra 2.5 días
+
+            Suponiendo que el estado por defecto es la hembra se calcula
+            la relación:
+
+            Macho = 2.5 / 1.9 Hembras = 1,315789474 Hembras
+
+            """
+            delta_madurez =100.0 / (randint(10,47) * 0.206 * 24.0)
+            #~  si es macho madura más rápido
+            if sexo.MACHO == self.sexo :
+                delta_madurez =  delta_madurez / 1.315789474
+
 
         """
         TODO Como afecta las condiciones climáticas al desarrollo de la
         pupa ?
         """
         self._espectativa_vida -= 0.1389
+        self._madurez += delta_madurez
         self._edad +=1
+
         return self
 
 class Adulto(AeAegypti) :

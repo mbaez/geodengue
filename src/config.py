6c03bba45919783cb20546c47e2ea7744cf27ad3
Maximiliano Báez González <mxbg.py@gmail.com>
2013-09-29 11:51:43 -0400

    se añaden las configuraciones de los datos climáticos

--- src/config.py
+++ src/config.py
@@ -25,3 +25,19 @@ GEOSERVER = {
     "password": "admin",
     "workspace": "geodengue"
 }
+
+#~ URL = 'http://www.tutiempo.net/tiempo/Asuncion_Aeropuerto/SGAS.htm?datos=por-horas'
+LOCALIDADES_HORA ={
+    "Asuncion" : 'tiempo/Asuncion_Aeropuerto/SGAS.htm?datos=por-horas'
+}
+# Datos utilizads para contruir los queryparams para realizar el get
+API_DATA = {
+    "appid" : "2d9be00662629ff5c269672af48013d8",
+    "type" : "day",
+    "id" : "3439389",
+    "mode" : "json"
+}
+# URLs de las fuentes de información de datos climaticos
+#~ API_URL = "http://api.openweathermap.org/data/2.5";
+API_URL = "http://localhost/geodengue/api.openweathermap.json";
+TUTIEMPO_URL = 'http://www.tutiempo.net/'

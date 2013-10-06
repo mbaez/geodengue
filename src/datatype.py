0bfb9607fbd04b6911f10a94626102c3f22d10eb
Maximiliano Báez González <mxbg.py@gmail.com>
2013-10-05 21:12:47 -0400

    Se añade el enum para representar la franja horaria

--- src/datatype.py
+++ src/datatype.py
@@ -10,22 +10,7 @@ class Enum(set):
             return name
         raise AttributeError
 
-import warnings
 
-def deprecated(func):
-    """
-    This is a decorator which can be used to mark functions
-    as deprecated. It will result in a warning being emmitted
-    when the function is used.
-    """
-    def newFunc(*args, **kwargs):
-        warnings.warn("Call to deprecated function %s." % func.__name__,
-                      category=DeprecationWarning)
-        return func(*args, **kwargs)
-    newFunc.__name__ = func.__name__
-    newFunc.__doc__ = func.__doc__
-    newFunc.__dict__.update(func.__dict__)
-    return newFunc
 """
 Enum que representa los estados por lo cuales atravieza el individuo.
 """
@@ -45,4 +30,9 @@ Zonas = Enum(["OPTIMA", "BUENA", "NORMAL", "MALA", "PESIMA"])
 Caracterización de las zonas
 """
 Clima = Enum(["FRIO", "FRESCO", "NORMAL", "CALIDO", "CALUROSO"])
+
+"""
+Caracterización de las franjas horarias
+"""
+Horario = Enum(["MADUGRADA", "MADUGRADA_MANHANA", "MANHANA", "TARDE_NOCHE", "TARDE", "NOCHE"])
 

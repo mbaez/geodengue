1a3611c1beee232f25952e8fa5735f2be3ea60eb
rbanuelos <robertobanuelos@gmail.com>
2013-10-06 14:40:47 -0300

    archivo para crear la tabla event_log. la tabla almacena los distintos eventos y estados por los cuales pasa cada individuo de la poblacion se anhade ademas el script para importar los datos generados a la tabla

---
+++ db/import_data_csv.sql
@@ -0,0 +1,3 @@
+﻿--agregar el path correcto $GEODENGUE_SRC/log/
+--el archivo de origen tiene un time stamp asociado ej> event_log2398423.log
+copy event_log from '/home/hduser/Documents/tesis/geodengue/src/log/event_log.log' DELIMITERS ',' CSV;

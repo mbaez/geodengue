a3bf791859332a751f91ee04a4b31da0ee4a1637
Maximiliano Báez González <mxbg.py@gmail.com>
2014-04-12 01:34:57 -0400

    Se añade el servicio que obtiene la lista de muestras existentes

--- src/rest_services.py
+++ src/rest_services.py
@@ -10,28 +10,40 @@ Este modulo define la interfaz de los servicios rest.
 __author__ = "Maximiliano Báez"
 __mail__ = "mxbg.py@gmail.com"
 
-#Se inicializa la api rest
+# Se inicializa la api rest
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'])
 def api_root():
     """Path por defecto de los servicios"""
     return ""
 
+
 @app.errorhandler(500)
 def internal_error(error):
     print error
-    return "500 error "+ str(error), 500
+    return "500 error " + str(error), 500
+
 
 @app.errorhandler(404)
 def not_found(error):
-    return "404 error",404
+    return "404 error", 404
+
+
+@app.route('/muestras', methods=['GET'])
+def get_lista_muestras():
+    controller = MuestrasController()
+    resp = controller.get_all_muestras()
+    print "parsing"
+    return jsonify(lista=resp)
+
 
 @app.route('/muestras/<muestra>/instantanea', methods=['POST'])
 def interpolate_idw(muestra):
-    gis = GisController(muestra);
-    col= row = 300
-    resp = gis.method_idw(col, row);
+    gis = GisController(muestra)
+    col = row = 300
+    resp = gis.method_idw(col, row)
     print "parsing"
     layer_name = gis.to_geoserver(resp, col, row, "inst")
     return jsonify(layer=layer_name)
@@ -39,8 +51,8 @@ def interpolate_idw(muestra):
 
 @app.route('/muestras/<muestra>/evolucionar', methods=['POST'])
 def evolutive(muestra):
-    col= row = 300
-    gis = GisController(muestra);
+    col = row = 300
+    gis = GisController(muestra)
     print "starting..."
     resp, muestras_evol = gis.method_evolutive()
     print "parsing"

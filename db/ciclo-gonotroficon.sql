ecd02e03b56a45847456e4705d9df11a5aa458e3
Maximiliano Báez González <mxbg.py@gmail.com>
2014-11-18 10:29:42 -0300

    Se actualizan los recursos del proyecto

---
+++ db/ciclo-gonotroficon.sql
@@ -0,0 +1,64 @@
+﻿select codigo,2,sum(tmp.cg_dias)/count(tmp)
+from (
+	select codigo, id_mosquito,cantidad_oviposicion, ciclo_gonotrofico as cg_dias
+	from evolucion_log
+	where ciclo_gonotrofico is not null
+	and ciclo_gonotrofico > 0
+	and estado='ADULTO'
+	and sexo = 'HEMBRA'
+	and se_alimenta = 'TRUE'
+	and cantidad_oviposicion > 0
+	group by codigo, id_mosquito,cantidad_oviposicion ,ciclo_gonotrofico
+) as tmp
+where  codigo= 'invierno-py-90'
+group by codigo
+--order by codigo
+
+UNION
+
+select codigo,1, sum(tmp.cg_dias)/count(tmp)
+from (
+	select codigo, id_mosquito,cantidad_oviposicion, ciclo_gonotrofico as cg_dias
+	from evolucion_log
+	where ciclo_gonotrofico is not null
+	and ciclo_gonotrofico > 0
+	and estado='ADULTO'
+	and sexo = 'HEMBRA'
+	and se_alimenta = 'TRUE'
+	and cantidad_oviposicion = 0
+	group by codigo, id_mosquito,cantidad_oviposicion ,ciclo_gonotrofico
+) as tmp
+where  codigo= 'invierno-py-90'
+group by codigo
+order by codigo
+
+
+select x.codigo, x.estado, round (sum(x.c - x.m)*1.0/count(x),2) as dias
+    from(
+        select codigo,estado, min(dia) as m, max(dia) as c
+        from evolucion_log tmp
+       where ciclo_gonotrofico is not null
+	and ciclo_gonotrofico > 0
+	and estado='ADULTO'
+	and sexo = 'HEMBRA'
+	--and se_alimenta = 'TRUE'
+	and cantidad_oviposicion =3
+        and exists (
+        select *
+        from evolucion_log
+        where id_mosquito = tmp.id_mosquito
+        --and id != tmp.id
+		and estado='ADULTO'
+		and sexo = 'HEMBRA'
+		and cantidad_oviposicion = tmp.cantidad_oviposicion +1
+        and codigo = tmp.codigo
+        )
+        group by codigo, id_mosquito, estado
+        --order by id_mosquito
+    ) x
+    where  x.codigo= 'invierno-py-90'
+    group by x.codigo,x.estado
+    
+
+
+    
---
+++ db/generacion.sql
@@ -0,0 +1,27 @@
+﻿select * from evolucion_log limit 10
+
+select temperatura, dia, count(*),generacion
+from evolucion_log
+where --id_mosquito_padre=0
+estado != 'ADULTO'
+and expectativa_de_vida != 0
+--and madurez != 0
+and generacion=3
+and codigo = 'invierno-py-90'
+group by dia,temperatura,generacion
+order by dia,generacion
+
+
+select temperatura, dia, count(*)
+from evolucion_log
+where --id_mosquito_padre=0
+estado = 'ADULTO'
+and expectativa_de_vida !=0
+and codigo = 'py-2010-full'
+--and madurez !=0
+group by dia,temperatura
+order by dia
+
+--delete from evolucion_log where codigo='pronostico-history-forecast'
+--delete from evolucion_log where codigo='alpha-pronostico'
+--delete from evolucion_log where codigo='alpha-015-pronostico'
